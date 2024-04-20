c2e0cc2e3cd8f7c1c533f8d756bcb11acaa556b6
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2024-04-20 09:23:43 +0400

    Fix double router issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './index.css';
 import App from './components/App';
@@ -10,9 +9,7 @@ const theme = createTheme();
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
